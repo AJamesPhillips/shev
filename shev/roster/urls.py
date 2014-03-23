49af0a8b78d2d48d79a6b942eaf83ad67692e8a3
Administrator <b.rowlingson@gmail.com>
2014-03-23 14:42:51 +0000

    dummy day view/url

--- shev/roster/urls.py
+++ shev/roster/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('shev.roster.views',
     url(r'^about/', 'about', name='about'),
+    url(r'^day/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', 'day', name='day'),
+# future:
+#    url(r'^month/(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'month', name='month'),
+#    url(r'^day/(?P<year>\d{4})/$', 'year', name='year'),
     url(r'^$', 'home', name='home')
 )
--- shev/roster/views.py
+++ shev/roster/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import get_object_or_404
+from . import models
+from datetime import datetime
 
 def home(request):
     return HttpResponse('<h1>Hello World</h1>')
 
 def about(request):
     return HttpResponse("<h1>Whatchew talking about?</h1>")
+
+def day(request, day, month, year):
+    the_day = get_object_or_404(models.Day, day = datetime(int(year),int(month),int(day)))
+    return HttpResponse("<h1>%s %s %s</h1>" % (day, month, year))
