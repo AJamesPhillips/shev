69f2608ebb8bb16d9de39fd9c4cf96bafad40125
Administrator <b.rowlingson@gmail.com>
2014-03-23 14:52:28 +0000

    simple working day view

--- shev/roster/views.py
+++ shev/roster/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response
 from . import models
 from datetime import datetime
 
@@ -11,4 +11,6 @@ def about(request):
 
 def day(request, day, month, year):
     the_day = get_object_or_404(models.Day, day = datetime(int(year),int(month),int(day)))
-    return HttpResponse("<h1>%s %s %s</h1>" % (day, month, year))
+    the_shifts = the_day.shift_set.all()
+    return render_to_response("day.html",{'day': the_day, 'shifts': the_shifts})
+    return HttpResponse("<h1>%s %s %s</h1> %s" % (day, month, year, the_day))
