694fe732813ba4eb9ae2f4e27ada15fbc9ec46a7
Alexander James Phillips <ajp.global@gmail.com>
2014-03-23 10:46:53 +0000

    Add redirect for root

--- shev/urls.py
+++ shev/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+
 admin.autodiscover()
 
 import roster
 
 urlpatterns = patterns('',
+    url(r'^/', HttpResponseRedirect('/admin/') ),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^roster/', include('roster.urls'))
 )
