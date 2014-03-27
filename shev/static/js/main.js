b018f3c4776b21e945737feeb831dd73773088ab
Alexander James Phillips <ajp.global@gmail.com>
2014-03-27 09:34:21 +0000

    Add routing for javascript

--- shev/static/js/main.js
+++ shev/static/js/main.js
@@ -1,40 +1,18 @@
 $(document).ready(function(){
-    // define some custom datatable sorters
-    jQuery.fn.dataTableExt.oSort['btp-pre']  = function(val) {
-        var sortVal = parseInt(val, 10);
-        if (_.isNumber(sortVal) && (! _.isNaN(sortVal))) {
-            'do nothing';
-        } else if (val === 'Supernumerary') {
-            sortVal = 0;
-        } else {
-            sortVal = 1;
-        }
-        console.log(val, sortVal);
-        return sortVal;
-    };
-
-    jQuery.fn.dataTableExt.oSort['btp-asc']  = function(x,y) {
-        return ((x < y) ? -1 : ((x > y) ?  1 : 0));
-    };
-
-    jQuery.fn.dataTableExt.oSort['btp-desc'] = function(x,y) {
-        return ((x < y) ?  1 : ((x > y) ? -1 : 0));
-    };
-
-    $('.shifts').dataTable({
-        aaSorting: [[ 0, "desc" ]],
-        aoColumnDefs: [
-            { bSortable: true, aTargets: [ 0 ], sType: "btp",  },
-            { bSortable: true, aTargets: [ '_all' ] }
-        ],
-        // This isn't needed as we use jquery to hide the option
-        // aLengthMenu: [
-        //     [-1],
-        //     ["All"],
-        // ],
-        iDisplayLength: -1,
+    // Routing
+    var Router = Backbone.Router.extend({
+        routes: {
+            'roster/overview/': 'overview',
+            'roster/day/:year/:month/:day/': 'day',
+        },
+        overview: function () {
+            window.overview_view()
+        },
+        day: function (year, month, day) {
+            window.day_view()
+        },
     });
-    $('.dataTables_length').hide();
-    $('.dataTables_info').hide();
-    $('.dataTables_paginate').hide();
+    var router = new Router();
+    // we're not using backbone the way it was intended but it works
+    Backbone.history.loadUrl(window.location.pathname)
 });
---
+++ shev/static/js/views/overview.js
@@ -0,0 +1,3 @@
+window.overview_view = function () {
+    // perhaps highlight an day where number of staff is under N
+};
